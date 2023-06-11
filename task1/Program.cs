ab3de33c85aeb9410530a65ee6a27dc16326830e
Mikhail <M_I_X_A_I_L_1992@mail.ru>
2023-06-11 22:44:02 +0600

    task1 is complete

---
+++ task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter first number: ");
+int nub1 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Enter second number: ");
+int nub2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+if (nub1 > nub2)
+{
+    Console.WriteLine("nub1 is max");
+}
+else 
+{
+    Console.WriteLine("nub2 is max");
+}
